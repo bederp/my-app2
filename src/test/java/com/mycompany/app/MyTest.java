f66251078f7d49cecf97bb4b5f754668d3e6743b
Paweł Beder <voyager112@gmail.com>
2018-10-14 12:40:17 +0200

    Initial commit

---
+++ src/test/java/com/mycompany/app/MyTest.java
@@ -0,0 +1,17 @@
+package com.mycompany.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assert.assertTrue(!false);
+    }
+}
